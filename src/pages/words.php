1957d56c8c7a30b0729d617eb962ca073db47b9b
Macghandley <macghandley@gmail.com>
2025-06-16 00:08:51 -0700

    Add files via upload
    Hopeful to remove bug when you have no words

--- src/pages/words.php
+++ src/pages/words.php
@@ -1,19 +1,14 @@
 <?php
-declare(strict_types = 1);           // Use strict types
+declare(strict_types = 1);         
 
 require_login();
-$id = $_SESSION['id'] ?? false; // Retrieve member_id from session
-if (!$id) {// If no valid id
-    include APP_ROOT . '/src/pages/page-not-found.php';  // Page not found
+$id = $_SESSION['id'] ?? false; 
+if (!$id) {
+    include APP_ROOT . '/src/pages/page-not-found.php';  
 }
 
 $words = $cms->getWord()->getAllFromMember($id);
-if(!$words)
-{                                        
-    include APP_ROOT . '/src/pages/page-not-found.php';  // Page not found
-}
 
-//var_dump(value: $words);
-$data['words']    = $words;
+$data['words'] = $words;
 
 echo $twig->render('words_display.html', $data);         
